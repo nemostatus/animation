74cb6079c3b7b4501c1ba289f50b5899867b8ae5
nemostatus <mattcorralejo1@gmail.com>
2021-02-03 06:22:30 -0800

    continuing to follow along with blog,will review and work with different sprite sheets and events

--- index.js
+++ index.js
@@ -7,17 +7,33 @@ const scale = 2
 const width = 16
 const height = 18
 const scaledWidth = width* scale
-const scaledHeight = height * scale,width,height
+const scaledHeight = height * scale
 
-function drawFrame(frameX, frameY, canvasX, canvasY){
-ctx.drawImage(img, frameX,frameY * height, canvasX, canvasY, scaledWidth, scaledHeight)
-}
+function drawFrame(frameX, frameY, canvasX, canvasY) {
+    ctx.drawImage(img,
+                  frameX * width, frameY * height, width, height,
+                  canvasX, canvasY, scaledWidth, scaledHeight);
+  }
 function init(){
 //     ctx.drawImage(img, 0, 0, width, height, 0, 0, scaledWidth, scaledHeight);
 //   ctx.drawImage(img, width, 0, width, height, scaledWidth, 0, scaledWidth, scaledHeight);
 //   ctx.drawImage(img, width * 2, 0, width, height, scaledWidth * 2, 0, scaledWidth, scaledHeight);
-drawFrame(0, 0, 0, 0);
-drawFrame(1, 0, scaledWidth, 0);
-drawFrame(0, 0, scaledWidth * 2, 0);
-drawFrame(2, 0, scaledWidth * 3, 0);
+// drawFrame(0, 0, 0, 0);
+// drawFrame(1, 0, scaledWidth, 0);
+// drawFrame(0, 0, scaledWidth * 2, 0);
+// drawFrame(2, 0, scaledWidth * 3, 0);
+window.requestAnimationFrame(step);
+}
+
+const cycleLoop= [0,1,0,2]
+let currentLoopIndex = 0
+
+function step(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0)
+    currentLoopIndex++
+    if(currentLoopIndex>=cycleLoop.length){
+        currentLoopIndex = 0
+    }
+    window.requestAnimationFrame(step);
 }
