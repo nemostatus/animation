8b0d519034e23db45b210a4dc157e50f744b8fdc
nemostatus <mattcorralejo1@gmail.com>
2021-02-03 05:10:14 -0800

    sprite renders

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-let image = new Image()
+let img = new Image()
+img.src = 'https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png';
+img.onload = () => {init()}
+let canvas = document.querySelector('canvas')
+let ctx= canvas.getContext('2d')
+
+function init(){
+    ctx.drawImage(img,0,0,16,18,0,0,16,18)
+}
